cafe892d760218fd01fa9c7d4eced0afb7ae2f5b
Manuel García Cremades <mgarcia.severodam@hotmail.com>
2022-02-09 16:08:29 +0100

    pruebas

---
+++ DepositoCombustible/pruebas/DepositoCombustibleTest.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DepositoCombustibleTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetDepositoNivel() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	void testGetDepositoMax() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	void testEstaVacio() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	void testEstaLleno() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	void testFill() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	void testConsumir() {
+		fail("No implementado aun");
+	}
+
+}
